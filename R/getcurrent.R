ce5d3667bb7eb4b0b351ee8e77ecc74fcac8cf59
fcluzeau <francois.cluzeau@gmail.com>
2016-07-25 17:22:42 +0200

    Update getcurrent.R

--- R/getcurrent.R
+++ R/getcurrent.R
@@ -5,9 +5,6 @@
 #' @param ticker stock ticker symbol. E.g. "GOOG".
 #' @export
 getcurrent <- function(ticker="GOOG",rom = "2013-01-01", to=Sys.time()){
-  mydata <- yahoodata(ticker, from, to);
- d<-dbeta(mydata,1,1);
- return(d);
     url <- paste('http://download.finance.yahoo.com/d/quotes.csv?s=',ticker,'&f=sl1d1t1n&e=.csv',sep="");
     mydata <- read.csv(url,header=FALSE);
     names(mydata) <- c("Symbol","Value","Date","Time","Name");
