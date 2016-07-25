26475435bccd63bcc933084ec37b0e5ecdefaeba
fcluzeau <francois.cluzeau@gmail.com>
2016-07-25 12:34:24 +0200

    Update smoothplot.R

--- R/smoothplot.R
+++ R/smoothplot.R
@@ -9,5 +9,5 @@
 #' @export
 smoothplot <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
   mydata <- yahoodata(ticker, from, to);
-  qplot(Date, Close, data = mydata, geom = c("line", "smooth"));  
+  qplot(Date, Close, data = mydata, geom = c("point", "smooth"));  
 }
