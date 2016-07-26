f5e65ea5abe1221418430034f1812f761553f567
fcluzeau <francois.cluzeau@gmail.com>
2016-07-26 15:49:54 +0200

    Update getMoyenne.R

--- R/getMoyenne.R
+++ R/getMoyenne.R
@@ -1,4 +1,4 @@
-getMoyenne <- function(ticker="GOOG",rom = "2013-01-01", to=Sys.time()){
+getMoyenne <- function(ticker="GOOG",from = "2013-01-01", to=Sys.time()){
      mydata <- yahoodata(ticker, from, to);
     names(mydata) <- c("Symbol","Value","Date","Time","Name");
     moyenne<-mean(mydata$Value);
