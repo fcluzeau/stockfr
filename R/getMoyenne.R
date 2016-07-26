b0ec3e041af59bbfba9a4e7a79265db238c2aa01
fcluzeau <francois.cluzeau@gmail.com>
2016-07-26 15:47:34 +0200

    Update getMoyenne.R

--- R/getMoyenne.R
+++ R/getMoyenne.R
@@ -1,6 +1,5 @@
 getMoyenne <- function(ticker="GOOG",rom = "2013-01-01", to=Sys.time()){
-    url <- paste('http://download.finance.yahoo.com/d/quotes.csv?s=',ticker,'&f=sl1d1t1n&e=.csv',sep="");
-    mydata <- read.csv(url,header=FALSE);
+     mydata <- yahoodata(ticker, from, to);
     names(mydata) <- c("Symbol","Value","Date","Time","Name");
     moyenne<-mean(mydata$Value);
     return(moyenne);
