759b3b4ff8f7944ea0e8295db12912027ebbf169
fcluzeau <francois.cluzeau@gmail.com>
2016-07-26 16:18:39 +0200

    Update getVariance.R

--- R/getVariance.R
+++ R/getVariance.R
@@ -1,1 +1,6 @@
-variance
+getVariance <- function(ticker="GOOG",from = "2013-01-01", to=Sys.time()){
+     mydata <- yahoodata(ticker, from, to);
+    names(mydata) <- c("Symbol","Value","Date","Time","Name");
+    var<-var(mydata$Value);
+    return(var);
+}
