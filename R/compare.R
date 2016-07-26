6a54b6094f7d4568bfacaca6503c3c21aa6721b6
fcluzeau <francois.cluzeau@gmail.com>
2016-07-26 13:23:56 +0200

    Update compare.R

--- R/compare.R
+++ R/compare.R
@@ -1,1 +1,7 @@
-knxjBOQ
+compare<-function(ticker="GOOG",rom = "2013-01-01", to=Sys.time()){
+getcur<-getcurrent(ticker="GOOG",rom = "2013-01-01", to=Sys.time())
+mydata <- yahoodata(ticker, from, to);
+if(ymin<ymax){ fill="green"}; 
+else{fill='red'};
+ggplot(data = mydata, ymin=getcur, aes(Date, ymax=High)) + geom_ribbon(color="black",fill, alpha=0.5) + ylim(range(mydata$Close));
+}
