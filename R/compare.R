d419485e4dd185ef09fc187b41681c1556b2aeec
fcluzeau <francois.cluzeau@gmail.com>
2016-07-26 13:30:52 +0200

    Delete compare.R

--- R/compare.R
+++
@@ -1,7 +0,0 @@
-compare<-function(ticker="GOOG",rom = "2013-01-01", to=Sys.time()){
-getcur<-getcurrent(ticker="GOOG",rom = "2013-01-01", to=Sys.time())
-mydata <- yahoodata(ticker, from, to);
-if(getcur$Value<mydata$Close){ fill="green"}
-else{fill="red"}
-ggplot(data = mydata, ymin=getcur, aes(Date, ymax=High)) + geom_ribbon(color="black",fill, alpha=0.5) + ylim(range(mydata$Close));
-}
