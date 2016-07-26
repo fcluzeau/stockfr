13e25884b6883ae739191fdf846ac7e141da1381
fcluzeau <francois.cluzeau@gmail.com>
2016-07-26 17:16:22 +0200

    Update plotwrapper.R

--- R/plotwrapper.R
+++ R/plotwrapper.R
@@ -25,6 +25,7 @@ plotwrapper <- function(type=c("smoothplot", "highlowplot", "areaplot"), ticker=
 	if(isTRUE(current)){
 		currentvalue <- getcurrent(ticker)$Value
 		myplot <- myplot + geom_hline(yintercept = currentvalue, colour = "red", linetype = 2, size = 0.8);	
+		myplot <- myplot + geom_text(x=-Inf, y = currentvalue, size=4, label = paste("Moyenne en devise locale:", currentvalue), hjust = -1, vjust = -0.5, color="blue");
 	}
 	
 		if(isTRUE(moyenne)){
@@ -33,8 +34,8 @@ plotwrapper <- function(type=c("smoothplot", "highlowplot", "areaplot"), ticker=
 		skewness<-getSkewness(ticker, from, to)
 		kurtosis<-getKurtosis(ticker, from, to)
 		myplot <- myplot + geom_hline(yintercept = moyenne, colour = "blue", linetype = 2, size = 0.8);	
-		myplot <- myplot + geom_text(x=-Inf, y = moyenne, size=5, label = paste("Moyenne en devise locale:", moyenne,"; Variance:", var), hjust = -1, vjust = -0.5, color="blue");	
-		myplot <- myplot + geom_label(mapping = NULL,  size=5, label = paste(" Skewness:", skewness,"; Kurtosis:", kurtosis), hjust = -1, vjust = -0.5, color="blue");
+		myplot <- myplot + geom_label(x=-Inf, (y = moyenne/1.12), size=4, label = paste("Moyenne en devise locale:", moyenne,"; Variance:", var), hjust = -1, vjust = -0.5, color="blue");	
+		myplot <- myplot + geom_label(x=-Inf, y = (moyenne/1.15),  size=4, label = paste(" Skewness:", skewness,"; Kurtosis:", kurtosis), hjust = -1, vjust = -0.5, color="blue");
 	}
 	#make sure to print the plot
 	print(myplot);
