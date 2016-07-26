c74a4d33b2d943b82fc8776458cea1467d281408
fcluzeau <francois.cluzeau@gmail.com>
2016-07-26 17:36:36 +0200

    Update plotwrapper.R

--- R/plotwrapper.R
+++ R/plotwrapper.R
@@ -10,7 +10,7 @@
 #' @param current include the current price of this stock. TRUE/FALSE.
 #' @import ggplot2
 #' @export
-plotwrapper <- function(type=c("smoothplot", "highlowplot", "areaplot"), ticker="GOOG", from="2013-01-01", to=Sys.time(), current=FALSE, moyenne=FALSE){
+plotwrapper <- function(type=c("smoothplot", "highlowplot", "areaplot"), ticker="GOOG", from="2013-01-01", to=Sys.time(), current=FALSE, moyenne=FALSE, variance=FALSE, skewness=FALSE, kurtosis=FALSE){
 	type <- match.arg(type);
 	myplot <- switch(type,
 		smoothplot = smoothplot(ticker, from, to),
@@ -30,12 +30,21 @@ plotwrapper <- function(type=c("smoothplot", "highlowplot", "areaplot"), ticker=
 	
 		if(isTRUE(moyenne)){
 		moyenne <- getMoyenne(ticker, from, to)
-		var<-getVariance(ticker, from, to)
-		skewness<-getSkewness(ticker, from, to)
-		kurtosis<-getKurtosis(ticker, from, to)
 		myplot <- myplot + geom_hline(yintercept = moyenne, colour = "blue", linetype = 2, size = 0.8);	
-		myplot <- myplot + geom_label(x=-Inf,y = (moyenne/1.1), size=4, label = paste("Moyenne en devise locale:", moyenne,"; Variance:", var), hjust = -1, vjust = -0.5, color="blue");	
-		myplot <- myplot + geom_label(x=-Inf, y = (moyenne/1.15),  size=4, label = paste(" Skewness:", skewness,"; Kurtosis:", kurtosis), hjust = -1, vjust = -0.5, color="blue");
+		myplot <- myplot + geom_label(x=-Inf,y = (moyenne/1.05), size=4, label = paste("Moyenne en devise locale:", moyenne), hjust = -1, vjust = -0.5, color="blue");
+		}
+		
+		if(isTRUE(variance)){
+		var<-getVariance(ticker, from, to)
+		myplot <- myplot + geom_label(x=-Inf,y = (moyenne/1.1), size=4, label = paste("Variance:", var), hjust = -1, vjust = -0.5, color="blue");	
+		}
+		if(isTRUE(skewness)){
+		skewn<-getSkewness(ticker, from, to)
+		myplot <- myplot + geom_label(x=-Inf, y = (moyenne/1.15),  size=4, label = paste("Skewness:", skewn), hjust = -1, vjust = -0.5, color="blue");
+		}
+		if(isTRUE(kurtosis)){
+		kurto<-getKurtosis(ticker, from, to)	
+		myplot <- myplot + geom_label(x=-Inf, y = (moyenne/1.2),  size=4, label = paste("Kurtosis:", kurto), hjust = -1, vjust = -0.5, color="blue");	
 	}
 	#make sure to print the plot
 	print(myplot);
