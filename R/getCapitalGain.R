13c8b6f5c692d5e049591f7a883c16c1eb9ed004
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 13:08:18 +0200

    Update getCapitalGain.R

--- R/getCapitalGain.R
+++ R/getCapitalGain.R
@@ -1,4 +1,9 @@
-getCapitalGain<-function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
+getCapitalGain <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
   mydata <- yahoodata(ticker, from, to);
-  
+  names(mydata) <- c("Symbol","Value","Date","Time","Name");
+  num<-dim(mydata)[1]
+  ase1 <- mydata[1,2];
+  ase2<- mydata[num,2];
+  gain<- (ase1-ase2)/ase2;
+  return(gain);
 }
