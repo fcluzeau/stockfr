5029828197fa7fad521a981a058e0b5e3aeab01d
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 21:20:51 +0200

    Delete getPlotCapitalGain.R

--- R/getPlotCapitalGain.R
+++
@@ -1,5 +0,0 @@
-getPlotCapitalGain<- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
-  mydata <- getCapitalGainMonth(ticker, from, to);
-  names(mydata) <- c("Value","Date");
-qplot(Date, Value, data = mydata, geom = c("line", "smooth"));  
-}
