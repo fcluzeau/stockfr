3ff728ddece24d1e192c899aae639245868e9a21
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 13:42:21 +0200

    Update getSkewness.R

--- R/getSkewness.R
+++ R/getSkewness.R
@@ -2,5 +2,6 @@ getSkewness<- function(ticker="GOOG",from = "2013-01-01", to=Sys.time()){
      mydata <- yahoodata(ticker, from, to);
     names(mydata) <- c("Symbol","Value","Date","Time","Name");
     skewn<-skewness(mydata$Value);
+    skewn<-round(skewn,5);
     return(skewn);
 }
