d7d230e51505feb6ddfbf936f0555b396f9f6839
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 22:18:00 +0200

    Update getPlotCapitalGain.R

--- R/getPlotCapitalGain.R
+++ R/getPlotCapitalGain.R
@@ -1,6 +1,6 @@
 
 getPlotCapitalGain<- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
-mydata <- getCapitalGainMonth(ticker, from, to);;
+mydata <- getCapitalGainMonth(ticker, from, to);
 d=as.data.frame(mydata);
 qplot(Date, Value, data=d, geom = c("line", "smooth")) 
 }
