5b687c27ee6ae33a459d570797e2f01ee4c32372
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 21:47:23 +0200

    Update getPlotCapitalGain.R

--- R/getPlotCapitalGain.R
+++ R/getPlotCapitalGain.R
@@ -2,7 +2,6 @@
 getPlotCapitalGain<- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
 mydata <- getCapitalGainMonth(ticker, from, to);
 names(mydata) <- c("Value","Date");
-date<-mydata$Date;
-valeur<-mydata$Value;
-plot(date, valeur); 
+c=as.data.frame(mydata);
+plot(c$Date, c$Value); 
 }
