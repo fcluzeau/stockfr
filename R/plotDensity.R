cc597968d1b88a4906f723f9d2364365afe26256
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 11:35:33 +0200

    Update plotDensity.R

--- R/plotDensity.R
+++ R/plotDensity.R
@@ -1,4 +1,8 @@
-
-d <- density(mtcars$mpg)
-plot(d, main="Kernel Density of Miles Per Gallon")
+plotDensity <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
+  mydata <- yahoodata(ticker, from, to);
+  
+ d <- density(mydata$Value)
+plot(d, main="Répartition des valeurs")
 polygon(d, col="red", border="blue") 
+}
+
