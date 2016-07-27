945d0ccc2fa598f16207b800be996f88f123701c
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 22:37:22 +0200

    Update getCapitalGainMonth.R

--- R/getCapitalGainMonth.R
+++ R/getCapitalGainMonth.R
@@ -5,7 +5,7 @@ names(mydata) <- c("Symbol","Value","Date","Time","Name");
 num<-dim(mydata)[1];
 gain<-matrix(ncol=2,nrow=num-1);
 for(i in 1:num-1){
-gain[i,1]<- ((mydata[i,2]-mydata[i+1,2])/mydata[i,2]);
+gain[i,1]<- 100*((mydata[i,2]-mydata[i+1,2])/mydata[i,2]);
 gain[i,2]<-mydata[i+1,3];}
 gain<-as.data.frame(gain);
 colnames(gain)<-c("Value","Date");
