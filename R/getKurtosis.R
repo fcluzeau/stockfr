9177e0c4b7b8ee5af320cf5aaf94bf604162dd93
fcluzeau <francois.cluzeau@gmail.com>
2016-07-27 13:41:09 +0200

    Update getKurtosis.R

--- R/getKurtosis.R
+++ R/getKurtosis.R
@@ -2,5 +2,6 @@ getKurtosis<-function(ticker="GOOG",from = "2013-01-01", to=Sys.time()){
      mydata <- yahoodata(ticker, from, to);
     names(mydata) <- c("Symbol","Value","Date","Time","Name");
     kurt<-kurtosis(mydata$Value);
+    kurt<-round(kurt,5);
     return(kurt);
 }
