36cc6d1f9cbb2d004ee2f34904c675b77cc0cc43
fcluzeau <francois.cluzeau@gmail.com>
2016-07-28 12:02:06 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -1,12 +1,10 @@
 getPortefeuilleValue<-function(ticker=c(a1,"GOOG",a2,"ACA.PA"),from="2013-01-01", to=Sys.time()){
 ticker=getPortefeuille();
+mydata<-matrix(ncol=(dim(ticker)/2+2),nrow=dim(yahoodata("GOOG", from, to))[1]);
+mydatf<-matrix(ncol=3, nrow=dim(yahoodata("GOOG", from, to))[1]);
 for(i in 1:(dim(ticker)/2)){
 mydatai<-yahoodata(ticker[i*2], from, to);
 names(mydatai)<-c("Symbol","Value","Date","Time","Name");
-mydata<-matrix(ncol=(dim(ticker)/2+2),nrow=dim(mydatai)[1]);
-mydatf<-matrix(ncol=3, nrow=dim(mydatai)[1]);
-mydata<-matrix(ncol=(dim(ticker)/2+2),nrow=dim(mydatai)[1]);
-mydatf<-matrix(ncol=3,nrow=dim(mydatai)[1]);
 mydata[,i]<-ticker[(i-1)*2+1]*mydatai[,2];
 }
 mydata[,(dim(ticker)/2+1)]<-mydatai[,3];
