49beebc165ad2a44f246fc3c2adf0042b8fc2f10
fcluzeau <francois.cluzeau@gmail.com>
2016-07-28 16:21:07 +0200

    Update getPortefeuille.R

--- R/getPortefeuille.R
+++ R/getPortefeuille.R
@@ -1,5 +1,7 @@
 getPortefeuille<-function(){
-portefeuille<-vector(2,"ACA.PA",3,"MC.PA");
+action<-c("ACA.PA","MC.PA");
+nombre<-c(2,3);
+portefeuille<-matrix(c(action,nombre), ncol=2, nrow=2, byrow=T);
 
 return(portefeuille);
 }
