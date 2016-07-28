d68f77e611faaf985abf0943394e7fcd1aafeb39
fcluzeau <francois.cluzeau@gmail.com>
2016-07-28 16:33:19 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -5,7 +5,7 @@ mydatf<-matrix(ncol=3, nrow=dim(yahoodata("ACA.PA", from, to))[1]);
 for(i in 1:(dim(ticker))){
 mydatai<-yahoodata(ticker[1,i], from, to);
 names(mydatai)<-c("Symbol","Value","Date","Time","Name");
-mydata[,i]<-ticker[2,i]*mydatai[,2];
+mydata[,i]<-ticker[2,i]*as.numeric(mydatai[,2]);
 }
 mydata[,(dim(ticker)+1)]<-mydatai[,3];
 mydata[,(dim(ticker)+2)]<-mydatai[,4];
@@ -13,8 +13,8 @@ mydata[,(dim(ticker)+2)]<-mydatai[,4];
 for(i in 1:(dim(ticker))){
 mydataf[,1]<-mydataf[,1]+mydata[,i];}
 
-mydataf[,2]<-mydata[,(dim(ticker)/2+1)];
-mydataf[,3]<-mydata[,(dim(ticker)/2+2)];
+mydataf[,2]<-mydata[,(dim(ticker)+1)];
+mydataf[,3]<-mydata[,(dim(ticker)+2)];
 mydatf<-as.data.frame(gainf);
 colnames(mydataf)<-c("Value","Date","Time");
 return(mydataf);
