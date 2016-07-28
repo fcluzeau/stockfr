dddf8d9d0381168fd1bcacfc9dc3647b6b20005b
fcluzeau <francois.cluzeau@gmail.com>
2016-07-28 16:25:39 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -1,16 +1,16 @@
-getPortefeuilleValue<-function(ticker=c(a1,"GOOG",a2,"ACA.PA"),from="2013-01-01", to=Sys.time()){
+getPortefeuilleValue<-function(from="2013-01-01", to=Sys.time()){
 ticker=getPortefeuille();
-mydata<-matrix(ncol=(dim(ticker)/2+2),nrow=dim(yahoodata("GOOG", from, to))[1]);
-mydatf<-matrix(ncol=3, nrow=dim(yahoodata("GOOG", from, to))[1]);
-for(i in 1:(dim(ticker)/2)){
-mydatai<-yahoodata(ticker[i*2], from, to);
+mydata<-matrix(ncol=(dim(ticker)[1]+2),nrow=dim(yahoodata("ACA.PA", from, to))[1]);
+mydatf<-matrix(ncol=3, nrow=dim(yahoodata("ACA.PA", from, to))[1]);
+for(i in 1:(dim(ticker))){
+mydatai<-yahoodata(ticker[1,i], from, to);
 names(mydatai)<-c("Symbol","Value","Date","Time","Name");
-mydata[,i]<-ticker[(i-1)*2+1]*mydatai[,2];
+mydata[,i]<-ticker[2,i]*mydatai[,2];
 }
-mydata[,(dim(ticker)/2+1)]<-mydatai[,3];
-mydata[,(dim(ticker)/2+2)]<-mydatai[,4];
+mydata[,(dim(ticker)+1)]<-mydatai[,3];
+mydata[,(dim(ticker)+2)]<-mydatai[,4];
 
-for(i in 1:(dim(ticker)/2)){
+for(i in 1:(dim(ticker))){
 mydataf[,1]<-mydataf[,1]+mydata[,i];}
 
 mydataf[,2]<-mydata[,(dim(ticker)/2+1)];
