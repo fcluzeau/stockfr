244ca28539080ff807b38ab49f9cc24b7fb68125
fcluzeau <francois.cluzeau@gmail.com>
2016-07-28 16:16:45 +0200

    Update plotDensity.R

--- R/plotDensity.R
+++ R/plotDensity.R
@@ -1,5 +1,5 @@
 plotDensity <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
-  if(ticker!=portefeuille){mydata <- yahoodata(ticker, from, to);
+  if(ticker!="portefeuille"){mydata <- yahoodata(ticker, from, to);
   names(mydata) <- c("Symbol","Value","Date","Time","Name");
  d <- density(mydata$Value)
 plot(d, main="Répartition des valeurs")
