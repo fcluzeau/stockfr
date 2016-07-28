2e127c890901c3b5171a1b6ff658f589cd548f35
fcluzeau <francois.cluzeau@gmail.com>
2016-07-28 19:53:16 +0200

    Update smoothplotPortefeuille.R

--- R/smoothplotPortefeuille.R
+++ R/smoothplotPortefeuille.R
@@ -1,6 +1,6 @@
 smoothplotPortefeuille<- function(from = "2013-01-01", to=Sys.time()){
 mydata<-getPortefeuilleValue(from,to);
-
-plot(mydata$Date, y = range(mydata$Value, na.rm=TRUE));
-
+x<-1:dim(mydata)[1];
+y<-mydata$Value
+plot(x,y)
 }
