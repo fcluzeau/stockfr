e4c0568731c532937bbfd6e6657203c91525afc7
fcluzeau <francois.cluzeau@gmail.com>
2016-07-28 16:44:24 +0200

    Update smoothplotPortefeuille.R

--- R/smoothplotPortefeuille.R
+++ R/smoothplotPortefeuille.R
@@ -1,4 +1,4 @@
 smoothplotPortefeuille<- function(from = "2013-01-01", to=Sys.time()){
 mydata<-getPortefeuilleValue(from,to);
-qplot(Date, Value, data = mydata, geom = c("line", "smooth"));
+qplot(mydata$Date, mydata$Value,  geom = c("line", "smooth"));
 }
