b4ec1bb69a8f0ab4184b5a90c25683ee2182b8c3
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 13:17:13 +0200

    Update plotDensityPortefeuilleByShare.R

--- R/plotDensityPortefeuilleByShare.R
+++ R/plotDensityPortefeuilleByShare.R
@@ -16,7 +16,7 @@ h<-hist(x, breaks=10, col="red", xlab="Variations mensuelles en %",
 xfit<-seq(min(x),max(x),length=40)
 yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
 yfit <- yfit*diff(h$mids[1:2])*length(x)
-lines(xfit, yfit, col="blue", lwd=2) }
+lines(xfit, yfit, col="blue", lwd=2) 
 }
 
 
