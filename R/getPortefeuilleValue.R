e16655738505f549a8a2cc8ec43a3abf49868dbe
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 09:44:35 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -1,17 +1,15 @@
 getPortefeuilleValue<-function(from="2013-01-01", to=Sys.time()){
-ticker=getPortefeuille();
-mydata<-matrix( nrow=dim(yahoodata(ticker[1,1], from, to))[1], ncol=4);
-for(i in 1:(dim(ticker))[1]){
-mydatai<-yahoodata(ticker[1,i], from, to);
+action<-c("ACA.PA","MC.PA");
+nombre<-c(2,3);
+portefeuille<-matrix(c(action,nombre), ncol=2, nrow=2, byrow=T);
+mydata<-matrix(numeric, nrow=dim(yahoodata(portefeuille[1,1], from, to))[1]);
+for(i in 1:2){
+mydatai<-yahoodata(portefeuille[1,i], from, to);
 colnames(mydatai)<-c("Symbol","Value","Date","Time","Name");
-for(j in 1:(dim(ticker))[1]){
-mydata[j,1]<-mydata[j,1]+as.numeric(ticker[2,i])*as.numeric(mydatai[j,2]);
+for(j in 1:2){
+mydata[j,1]<-mydata[j,1]+as.numeric(portefeuille[2,i])*as.numeric(mydatai[j,2]);
 }
 }
-mydata[,2]<-mydatai[,2];
-mydata[,3]<-mydatai[,3];
-mydata[,4]<-mydatai[,4];
-colnames(mydata)<-c("Value","Date","Time","Name");
-mydata<-as.data.frame(mydata)
+
 return(mydata);
 }
