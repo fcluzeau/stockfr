e36157cc8d326543577bf293b40e938f95ba713e
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 14:02:34 +0200

    Update plotDensityPortefeuilleByShare.R

--- R/plotDensityPortefeuilleByShare.R
+++ R/plotDensityPortefeuilleByShare.R
@@ -25,7 +25,7 @@ xfit<-seq(min(x),max(x),length=40)
 yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
 yfit <- yfit*diff(h$mids[1:2])*length(x)
 lines(xfit, yfit, col="blue", lwd=2) 
-h <- h + geom_hline(xintercept = moyenne, colour = "blue", linetype = 2, size = 0.8);
+h <- h + geom_vline(xintercept = moyenne, colour = "blue", linetype = 2, size = 0.8);
 }
 
 
