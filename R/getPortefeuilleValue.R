d8ac5563f9cfa36da5eb0995df04ab843862e758
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 10:25:37 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -2,13 +2,12 @@ getPortefeuilleValue<-function(from="2013-01-01", to=Sys.time()){
 action<-c("ACA.PA","MC.PA");
 nombre<-c(2,3);
 portefeuille<-matrix(c(action,nombre), ncol=2, nrow=2, byrow=T);
-m<-dim(yahoodata(portefeuille[1,1], from, to))[1];
-mydata<-numeric(m);
+mydata<-matrix(nrow=dim(yahoodata(portefeuille[1,1], from, to))[1], ncol=length(action));
 for(i in 1:2){
 mydatai<-yahoodata(portefeuille[1,i], from, to);
-colnames(mydatai)<-c("Symbol","Value","Date","Time","Name");
+mydataf<-c(mydatai$Date, mydatai$Close);
 for(j in 1:2){
-mydata[j]<-mydata[j]+as.numeric(portefeuille[2,i])*as.numeric(mydatai[j,2]);
+mydata[j]<-mydata[j]+as.numeric(portefeuille[2,i])*as.numeric(mydataf[j,2]);
 }
 }
 
