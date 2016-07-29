eb8c048476b0414d6c25c4ec4c0f1a13c2e78544
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 16:16:04 +0200

    Update getPortefeuille.R

--- R/getPortefeuille.R
+++ R/getPortefeuille.R
@@ -1,5 +1,5 @@
 getPortefeuille<-function(){
-newtable <- with(portefeille, data.frame(id=paste0("symbol_", Action), text=paste(Ticker, "-", Nombre)));
+newtable <- with(portefeuille, data.frame(id=paste0("symbol_", Action), text=paste(Ticker, "-", Nombre)));
 #action<-c("AC.PA","ACA.PA","AI.PA","AIR.PA","BN.PA","BNP.PA","CA.PA","CAP.PA","CS.PA","DG.PA","EI.PA","EN.PA","ENGI.PA","FP.PA","FR.PA","GLE.PA","KER.PA","LHN.PA","LI.PA","LR.PA","MC.PA","ML.PA");
 #nombre<-c(1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22); 
 #portefeuille<-matrix(nrow=2, ncol=length(action))
