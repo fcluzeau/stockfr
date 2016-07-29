9ed90522f24d1d07402443805eaa3e1dfb655d5b
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 14:44:47 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -15,6 +15,12 @@ myporte[j,2]<-myporte[j,2]*as.numeric(nombre[i]);
 myporte[,1]<-mydata$Date;
 colnames(myporte)<-c("Date","Close");
 myporte<-data.frame(myporte)
+num<-dim(myporte)[1]
+  ase1 <- myporte[1,2];
+  ase2<- myporte[num,2];
+  gain<- (ase1-ase2)/ase2;
+  gain<-round(gain,5);
 
-qplot(Date, Close, data = mydata, geom = c("line", "smooth"));  
+
+qplot(Date, Close, data = mydata, geom = c("line", "smooth"), xlab=paste("Gain du Capital:",gain,"%");  
 }
