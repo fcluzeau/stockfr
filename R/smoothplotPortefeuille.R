00e3b48f1707a3ebdfb7880c3deb1c90b9cb59d9
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 08:55:19 +0200

    Update smoothplotPortefeuille.R

--- R/smoothplotPortefeuille.R
+++ R/smoothplotPortefeuille.R
@@ -1,8 +1,5 @@
 smoothplotPortefeuille<- function(from = "2013-01-01", to=Sys.time()){
 mydata<-getPortefeuilleValue(from,to);
 
- ggplot(mydata, aes(x=mydata[,1], y=mydata[,2])) + 
- + geom_point(shape=1) + # Use hollow circles 
- + geom_smooth(method=lm) + # Add linear regression line  
- + theme(axis.text=element_text(size=1))  
+ ggplot(mydata, aes(x=mydata[,1], y=mydata[,2]))
 }
