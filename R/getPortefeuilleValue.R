34f7dd5a27c1703569110b329da3d3b689edbf3f
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 14:18:05 +0200

    Create getPortefeuilleValue.R

---
+++ R/getPortefeuilleValue.R
@@ -0,0 +1,19 @@
+getPortefeuilleValue<-function( from = "2013-01-01", to=Sys.time()){
+action<-getPortefeuille()[1,];
+nombre<-getPortefeuille()[2,];
+m<-yahoodata(action[1], from, to)
+myporte<-matrix( nrow=m , ncol=2);
+
+for(i in 1:length(action)){
+mydata <- yahoodata(action[i], from, to);
+for(j in 1:dim(mydata)[1]){
+myporte[j,2]<-nombre[i]*mydata$Close[j];
+}
+}
+
+myporte[,1]<-mydata$Date;
+colnames(myporte)<-c("Date","Close");
+myporte<-data.frame(myporte)
+
+qplot(Date, Close, data = mydata, geom = c("line", "smooth"));  
+}
