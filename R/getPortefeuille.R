320eea729ee546ee91225914b80da16b9c13a412
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 13:24:36 +0200

    Update getPortefeuille.R

--- R/getPortefeuille.R
+++ R/getPortefeuille.R
@@ -1,4 +1,4 @@
 getPortefeuille<-function(){
-portefeuille<-c("AC.PA","ACA.PA","AI.PA","AIR.PA","BN.PA","BNP.PA","CA.PA","CAP.PA","CS.PA","DG.PA","EI.PA","EN.PA","ENGI.PA","FP.PA","FR.PA","GLE.PA","KER.PA","LHN.PA","LI.PA","LR.PA","MC.PA","ML.PA","MT.PA","NOKIA.PA","OR.PA","ORA.PA","PUB.PA","RI.PA","RNO.PA","SAF.PA","SAN.PA","SGO.PA","SOLB.PA","SU.PA","SW.PA","TEC.PA","UG.PA","UL.PA","VIE.PA","VIV.PA");
+portefeuille<-c("AC.PA","ACA.PA","AI.PA","AIR.PA","BN.PA","BNP.PA","CA.PA","CAP.PA","CS.PA","DG.PA","EI.PA","EN.PA","ENGI.PA","FP.PA","FR.PA","GLE.PA","KER.PA","LHN.PA","LI.PA","LR.PA","MC.PA","ML.PA");
 return(portefeuille);
 }
