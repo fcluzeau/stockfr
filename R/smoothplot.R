ecf81d374bf5d710105212233873a58ef86f0187
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 11:44:33 +0200

    Update smoothplot.R

--- R/smoothplot.R
+++ R/smoothplot.R
@@ -13,36 +13,7 @@ smoothplot <- function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
   qplot(Date, Close, data = mydata, geom = c("line", "smooth"));  
 }
 else{
-
-mydata<-yahoodata("ACA.PA", from, to);
- 
- x <- mydata$Close;
-h<-hist(x, breaks=10, col="red", xlab="Miles Per Gallon",
-   main="Histogram with Normal Curve")
-xfit<-seq(min(x),max(x),length=40)
-yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
-yfit <- yfit*diff(h$mids[1:2])*length(x)
-lines(xfit, yfit, col="blue", lwd=2)
-g<-plot(mydata$Date, mydata$Close, main="Scatterplot of wt vs. mpg")
-
-mydata<-yahoodata("MC.PA", from, to);
- x <- mydata$Close;
-r<-hist(x, breaks=10, col="red", xlab="Miles Per Gallon",
-   main="Histogram with Normal Curve")
-xfit<-seq(min(x),max(x),length=40)
-yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
-yfit <- yfit*diff(h$mids[1:2])*length(x)
-lines(xfit, yfit, col="blue", lwd=2)
-t<-plot(mydata$Date, mydata$Close, main="Scatterplot of wt vs. mpg")
-
-
-attach()
-par(mfrow=c(2,2))
-g
-h
-r
-t
-}
-}
+ticke<-getPortefeuille();
+plotDensityPortefeuilleByShare(ticke, from, to);
 }
 }
