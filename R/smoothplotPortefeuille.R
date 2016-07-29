31b8f4321351ae1f2682e946126d4681c737d4be
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 11:46:02 +0200

    Delete smoothplotPortefeuille.R

--- R/smoothplotPortefeuille.R
+++
@@ -1,6 +0,0 @@
-smoothplotPortefeuille<- function(from = "2013-01-01", to=Sys.time()){
-mydata<-getPortefeuilleValue(from,to);
-colnames(mydata)<-c("Date","Close");
-mydata<-data.frame(mydata);
-qplot(Date, Close, data = mydata, geom = c("line", "smooth"));
-}
