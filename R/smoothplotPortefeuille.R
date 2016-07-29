8327875f0425278a40807d3b933cdca2a26e24d7
fcluzeau <francois.cluzeau@gmail.com>
2016-07-29 10:12:39 +0200

    Update smoothplotPortefeuille.R

--- R/smoothplotPortefeuille.R
+++ R/smoothplotPortefeuille.R
@@ -1,6 +1,5 @@
 smoothplotPortefeuille<- function(from = "2013-01-01", to=Sys.time()){
 mydata<-getPortefeuilleValue(from,to);
 x<-1:length(mydata);
-plot(x,mydata, xlab="x axis", ylab="y axis", main="my plot", ylim=c(min(mydata),max(mydata)), xlim=c(1,length(mydata)))+
-geom_point() 
+points(x, mydata, pch=16, col="green")
 }
