b689cc73e25b1ce67132bf9fe93ed7113a695e55
fcluzeau <francois.cluzeau@gmail.com>
2016-07-31 22:31:34 +0200

    Update plotwrapper.R

--- R/plotwrapper.R
+++ R/plotwrapper.R
@@ -23,10 +23,10 @@ plotwrapper <- function(type=c("smoothplot", "highlowplot", "areaplot", "plotDen
 		getPortefeuilleValue = getPortefeuilleValue(from, to),
 		stop("Unknown plot type:", type)
 	);
-	
-	if(type!="plotDensity" & type!="getPlotCapitalGain" & type!="densityGain" & type!="plotDensityPortefeuilleByShare" & type!="getPortefeuilleValue"){
+	if(type=smoothplot){myplot}
+	else if(type!="plotDensity" & type!="getPlotCapitalGain" & type!="densityGain" & type!="plotDensityPortefeuilleByShare" & type!="getPortefeuilleValue"){
 	#remove axis label date
-	myplot <- myplot + xlab("") + ylab(ticker);}
+	myplot <- myplot + xlab(Date) + ylab(ticker);}
 	else{myplot}
 	
 	if(isTRUE(current)){
