24037d57b99dfba25a128abf3e32a9c5642413b2
fcluzeau <francois.cluzeau@gmail.com>
2016-07-31 17:47:15 +0200

    Update densityGain.R

--- R/densityGain.R
+++ R/densityGain.R
@@ -1,15 +1,13 @@
 densityGain<-function(ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
 gainf<-getCapitalGainMonth(ticker, from, to);
-gainf<-as.numeric(gainf);
-moyenne<-mean(gainf)
-moyenne<-round(moyenne,5);
-moyennegeo<-round(getMoyenneGeometrique(gainf),5);
-x <- gainf;
-h<-hist(x, breaks=10, col="red", xlab=paste("Variations mensuelles en %; moyenne arithmétique:",moyenne,"%; moyenne géométrique:",moyennegeo,"%"),
+moyennegeo<-getMoyenneGeometrique(gainf);
+moyennear<-mean(gainf)
+x <- gainf$Value;
+h<-hist(x, breaks=10, col="red", xlab=paste("Variations mensuelles en %; moyenne arithmétique:",moyennear,"moyenne géométrique:",moyennegeo)
    main="Histogramme de la Répartition des Variations Mensuelle")
 xfit<-seq(min(x),max(x),length=40)
 yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
 yfit <- yfit*diff(h$mids[1:2])*length(x)
 lines(xfit, yfit, col="blue", lwd=2)
-abline(h=0,v=moyenne,col="black");
+abline(h=0,v=moyennear,col="black");
 }
