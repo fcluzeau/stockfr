253da6f0f3cbe88ea31b2f193f7ab8fa6b1206e3
fcluzeau <francois.cluzeau@gmail.com>
2016-07-31 17:08:15 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -22,11 +22,11 @@ num<-dim(myporte)[1]
   gain<- (ase1-ase2)/ase2;
   gain<-round(100*gain,5);
   
-  gainf<-matrix(ncol=2,nrow=floor((dim(myporte)[1]-1)/20));
+  gainf<-numeric(floor((dim(myporte)[1]-1)/20));
 for(i in 1:dim(myporte)[1]-1){
 gaini[i]<- 100*((myporte[i,2]-myporte[i+1,2])/myporte[i,2]);
 if((i/20)==floor(i/20)){
-gainf[(i/20),]<-gain[i,];
+gainf[(i/20)]<-gaini[i];
 }}
 moyenneredm<-round(mean(gainf),5);
 moyennegeoredm<-round(getMoyenneGeometrique(gainf),5);
