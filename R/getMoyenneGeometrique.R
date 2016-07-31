88b166c01fce3510980ec228495c0d07d387ad7f
fcluzeau <francois.cluzeau@gmail.com>
2016-07-31 18:32:33 +0200

    Update getMoyenneGeometrique.R

--- R/getMoyenneGeometrique.R
+++ R/getMoyenneGeometrique.R
@@ -1,7 +1,9 @@
 getMoyenneGeometrique<-function(vect){
 res<-1
+rf<-1/length(vect);
 for(i in 1:length(vect)){
-res=res*(1+vect[i])^(1/(length(vect)));
+res=res*(1+vect[i]);
 }
+res<-res^rf
 return(res-1)
 }
