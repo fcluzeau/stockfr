34b203c2deadd8569be1454c1be929e53ad2f134
fcluzeau <francois.cluzeau@gmail.com>
2016-08-04 01:38:43 +0200

    Update stocks.js

--- inst/www/stocks.js
+++ inst/www/stocks.js
@@ -6,14 +6,7 @@ Ext.onReady(function() {
   
   var today = new Date();
   
-  Ext.define('treePanel', {
-    extend: 'Ext.tree.Panel',
-    
-    requires: [
-        'Ext.data.TreeStore'
-    ],
-    xtype: 'check-tree',
-    
+  var treePanel = new Ext.tree.TreePanel({
     id: 'tree-panel',
     iconCls: 'chartIcon',
     title: 'by Index',
@@ -34,35 +27,30 @@ Ext.onReady(function() {
     lines: false,
     singleExpand: true,
     useArrows: true,
-    
-    initComponent: function(){
-
-        Ext.apply(this, {
-            store: {
-      xtype: 'checkbox',
+    store: {
       root: {
         expanded: true
       }
     },
-                sorters: [{
-                    property: 'leaf',
-                    direction: 'ASC'
-                }, {
-                    property: 'text',
-                    direction: 'ASC'
-                }]
-            }),
-    
     listeners: {
       itemdblclick: function(s, r){
         if(r.data.leaf){
           addWorkspace(r.data.id.substring(7));
         }
       },
-    
+      itemclick : function(s, r){
+        if(r.data.leaf){
+          var name = r.data.text.split(" - ");
+          var stock = name[0]
+          var company = name[1];
+          if(stock!=portefeuille){
+          Ext.getCmp("details-panel").update('<div class="detaildiv"> <h3>' + company + '</h3> Yahoo Finance: <a target="_blank" href="http://finance.yahoo.com/q?s=' + stock + '">'+stock+'</a></div>');
+        }   
+        
+        }
+      }
     }      
   });  
-  
     
   var myToolbar = Ext.create('Ext.toolbar.Toolbar', {
     "items" :['->',{
